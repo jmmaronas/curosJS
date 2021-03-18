cf2c1f454cc5fdda498e4d7666ce01b6267897bd
Juan Manuel <jmmaronas@gmail.com>
2021-03-18 15:47:20 -0300

    "pruebas carrito"

--- js/baseArticulos.js
+++ js/baseArticulos.js
@@ -1,16 +1,86 @@
 var arrayArtNenes = [
     {
         id : 0,
-        nombre : "vermuda",
-        img:  "image/ropa/nene/vermuda01.jpeg",
+        nombre : "Bermuda de jean",
+        img:  "image/ropa/nene/bermuda01.jpeg",
         precio : 499
     },
 
     {
         id : 1,
-        nombre : "vermuda",
-        img :  "image/ropa/nene/vermuda01.jpeg",
-        precio : 499
+        nombre : "Bermuda de baño",
+        img :  "image/ropa/nene/bermuda02.jpeg",
+        precio : 599
+    },
+
+    {
+        id : 2,
+        nombre : "Remera",
+        img :  "image/ropa/nene/remeracorta01.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 3,
+        nombre : "Remera",
+        img :  "image/ropa/nene/remeracorta02.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 4,
+        nombre : "",
+        img :  "image/ropa/nene/remeracorta03.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 5,
+        nombre : "Remera",
+        img :  "image/ropa/nene/remeracorta04.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 6,
+        nombre : "Remera",
+        img :  "image/ropa/nene/remeracorta05.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 7,
+        nombre : "Remera larga",
+        img :  "image/ropa/nene/remeralarga01.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 6,
+        nombre : "Buzo",
+        img :  "image/ropa/nene/buzo02.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 7,
+        nombre : "Buzo",
+        img :  "image/ropa/nene/buzo05.jpeg",
+        precio : 699
+    },
+
+    {
+        id : 8,
+        nombre : "Pantalon",
+        img :  "image/ropa/nene/pantalon01.jpeg",
+        precio : 1099
+    },
+
+    {
+        id : 9,
+        nombre : "Pantalon",
+        img :  "image/ropa/nene/pantalon02.jpeg",
+        precio : 1099
     }
 ];
 
--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,6 @@
 function cargarArt(p){
     var container = document.getElementById("artCont");
-    for(i=0; i<p.length;i++){
+    for(i=0; i< p.length;i++){
     
     container.innerHTML += 
     `<div class="artContainer col-6 col-md-4 col-lg-3">
@@ -14,23 +14,26 @@ function cargarArt(p){
         <strong>$<span class="precio">${p[i].precio}</span></strong>
     </div>
     <div class="artSelect">
-        <input type="button" value="Agregar" onclick="cargarCarrito(${p[i].id})">
+        <input class="button" type="button" value="Agregar" onclick="cargarCarrito(${p[i].id})">
     </div>
     </div> `
     }
     
 }
-
-var arrayCompra = []; 
+var local =0;
+var arrayCompra =[]; 
 function cargarCarrito(id){
     var compra = arrayArtNenes.filter(art => art.id == id);
     var artSeleccion = 0;
     arrayCompra.push(artSeleccion = new Carrito(compra[0].id, compra[0].nombre, compra[0].img, compra[0].precio));
+    localStorage.setItem('carr', JSON.stringify(arrayCompra));
+    local= localStorage.getItem("carr");
     console.log(arrayCompra);
+    console.log(local);
     return arrayCompra;
 }
 
-function mostrarCarrito(arrayObjetos = arrayCompra){
+function mostrarCarrito(arrayObjetos = localStorage.getItem("carr")){
     var carrito = document.getElementById("carrito");
     arrayObjetos.forEach(e => {    
         carrito.innerHTML += `
