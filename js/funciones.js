cd6b88ddf21a654a686f141601f912edc06bb492
Juan Manuel Maronas <jmmaronas@gmail.com>
2021-04-12 19:41:56 -0300

    final

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,8 @@
+// ANIMACION DEL LOGO AL CARGAR INDEX
+
 $(document).ready(()=>$("#logo").slideDown(2000));
+
+//GENERA LOS PRODUCTOS EN LA PAGINA
 
 function cargarArt(s){
     $.getJSON("../Json/baseArticulos.json", (resultado, estado) =>{
@@ -26,50 +30,48 @@ function cargarArt(s){
     });
 }
 
+// CARGAR LOS PRODUCTOS SELECCIONADOS EN EL STORAGE
 
-    var arrayCompra =[]; 
-    function cargarCarrito(id, arrayArt){
-        
-        $.getJSON("../Json/baseArticulos.json", (resultado, estado) =>{
-            if(estado = "success"){
-                let p = resultado[arrayArt];
-                let compra = p.filter(art => art.id == id);
-                let artSeleccion = 0;
-                let comprobar = JSON.parse(localStorage.getItem("carr"));
-                if(comprobar){
-                     arrayCompra=JSON.parse(localStorage.getItem("carr"));
-                }    
-                arrayCompra.push(artSeleccion = new Carrito(compra[0].id, compra[0].nombre, compra[0].img, compra[0].precio));
-                localStorage.setItem('carr', JSON.stringify(arrayCompra));
-                //alert("El producto fue agregado al carrito");
-                $("#artCont").prepend(`
-                    <div id="modal" width="320" height= "320">
-                        <h2>El producto fue agregado al carrito</h2>
-                    </div>`
-                );
-                
-                var winH = $(window).height();
-                var winW = $(window).width();
-                $("#modal").css({
-                    "background-color": "#d4cece",
-                    "position": "absolute",
-                    "width": "10rem",
-                    "top": winH/2,
-                    "left": winW/2,
-                    "border": "solid 1px #000000"
-                    }).slideUp(6000);
-            }
-        });
-    }
+var arrayCompra =[]; 
+function cargarCarrito(id, arrayArt){
+    
+    $.getJSON("../Json/baseArticulos.json", (resultado, estado) =>{
+        if(estado = "success"){
+            let p = resultado[arrayArt];
+            let compra = p.filter(art => art.id == id);
+            let artSeleccion = 0;
+            let comprobar = JSON.parse(localStorage.getItem("carr"));
+            if(comprobar){
+                    arrayCompra=JSON.parse(localStorage.getItem("carr"));
+            }    
+            arrayCompra.push(artSeleccion = new Carrito(compra[0].id, compra[0].nombre, compra[0].img, compra[0].precio));
+            localStorage.setItem('carr', JSON.stringify(arrayCompra));
+            //alert("El producto fue agregado al carrito");
+            $("#artCont").prepend(`
+                <div id="modal" width="320" height= "320">
+                    <h2>El producto fue agregado al carrito</h2>
+                </div>`
+            );
+            
+            var winH = $(window).height();
+            var winW = $(window).width();
+            $("#modal").css({
+                "background-color": "#d4cece",
+                "position": "absolute",
+                "width": "10rem",
+                "top": winH/2,
+                "left": winW/2,
+                "border": "solid 1px #000000"
+                }).slideUp(6000);
+        }
+    });
+}
 
-    function mostrarCarrito(arrayObjetos){
-        let comprobar = JSON.parse(localStorage.getItem("carr"));
-        
-        // if(comprobar === null){
-        
-        // }
-        // else{
-            arrayObjetos.forEach(e => {
+//GENERA LA PAGINA CON EL STORAGE 
+
+function mostrarCarrito(arrayObjetos){
+    let comprobar = JSON.parse(localStorage.getItem("carr"));
+        arrayObjetos.forEach(e => {
             $("#carrito").append(`
                 <div class="artContainer col-6 col-md-4 col-lg-3">
                     <div class="artImage">
@@ -85,77 +87,84 @@ function cargarArt(s){
                         <input class="button" type="button" value="Quitar" onclick="quitarArticulo(${e.id})">
                     </div>
                 </div> `);
-            });
-        }
+        });
+}
+
+// VACIA EL STORAGE 
+
+function vaciarCarrito(){
+    localStorage.clear();
+    location.reload();
+}
+
+//SUMA EL VALOR DE LOS PRODUCTOS SELECCIONADOS
+
+var total=0;
+function sumarProductos(objetosCarrito){
+    let comprobar = JSON.parse(localStorage.getItem("carr"));
     
-
-    function vaciarCarrito(){
-        localStorage.clear();
-        location.reload();
+    if(comprobar === null){
+    
     }
+    else{
+        var totales = document.getElementById("total");
+        objetosCarrito.forEach(e => {
+            total += e.precio;
+        })
+        totales.innerHTML += total;
+    }
+}
 
-    var total=0;
-    function sumarProductos(objetosCarrito){
-        let comprobar = JSON.parse(localStorage.getItem("carr"));
-        
-        if(comprobar === null){
-        
+//QUITA EL ARTICULO SELECCIONADO DEL STORAGE
+
+function quitarArticulo(id){
+    arrayC=JSON.parse(localStorage.getItem("carr"));
+    var elem = arrayC.filter(art => art.id === id);
+    var elemen = elem[0]; 
+    var bus=arrayC.indexOf(elemen);
+    arrayC.splice(bus,1);
+    localStorage.setItem('carr', JSON.stringify(arrayC));
+    location.reload();
+}
+
+//CAPTURA EL EVENTO Y GENERA UN FORM EN EL CARRITO
+
+$("#pedido").click(function(){
+    $("#formPedido").html(`
+    <form action="correo.php" method="POST" name="Formulario Consulta">
+        <fieldset>
+            <legend class="encabezado">Fomulario</legend>
+                <div class="form-group">                
+                    <label for="nombre">Nombres:</label>
+                    <input class="form-control input" type="text" value="" name="nombre" placeholder="Juan..." id="nombre">
+                </div>
+                <div class="form-group">
+                    <label for="apellido">Apellidos:</label>
+                    <input class="form-control input" type="text" name="apellido" placeholder="Matinez.." id="apellido">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input class="form-control input" type="email" name="email" placeholder="tu_mail@algo.com" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="telefono">Telefono:</label>
+                    <input class="form-control input" type="number" name="telefono" placeholder="011-132465.." id="telefono">
+                </div>
+                <input class="btn btn-primary button" type="submit" value="Enviar" name="enviar">
+                <input class="btn btn-primary button" type="reset" value="Restablecer">
+        </fieldset>
+    </form>`);
+    
+    $.ajax({            
+        data:  {"parametros": localStorage.getItem("carr")}, //datos que se envian a traves de ajax
+        url:   '../pages/correo.php', //archivo que recibe la peticion
+        type:  'post', //método de envio
+        beforeSend: function () {
+                $("#resultado").html("Procesando, espere por favor...");
+        },
+        success:  function (response) { //una vez que el archivo recibe el request lo procesa y lo devuelve
+                $("#resultado").html(response);
         }
-        else{
-            var totales = document.getElementById("total");
-            objetosCarrito.forEach(e => {
-                total += e.precio;
-            })
-            totales.innerHTML += total;
-        }
-    }
-
-    function quitarArticulo(id){
-        arrayC=JSON.parse(localStorage.getItem("carr"));
-        var elem = arrayC.filter(art => art.id === id);
-        var elemen = elem[0]; 
-        var bus=arrayC.indexOf(elemen);
-        arrayC.splice(bus,1);
-        localStorage.setItem('carr', JSON.stringify(arrayC));
-        location.reload();
-    }
-
-    $("#pedido").click(function(){
-        $("#formPedido").html(`
-        <form action="correo.php" method="POST" name="Formulario Consulta">
-            <fieldset>
-                <legend class="encabezado">Fomulario</legend>
-                    <div class="form-group">                
-                        <label for="nombre">Nombres:</label>
-                        <input class="form-control input" type="text" value="" name="nombre" placeholder="Juan..." id="nombre">
-                    </div>
-                    <div class="form-group">
-                        <label for="apellido">Apellidos:</label>
-                        <input class="form-control input" type="text" name="apellido" placeholder="Matinez.." id="apellido">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input class="form-control input" type="email" name="email" placeholder="tu_mail@algo.com" id="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="telefono">Telefono:</label>
-                        <input class="form-control input" type="number" name="telefono" placeholder="011-132465.." id="telefono">
-                    </div>
-                    <input class="btn btn-primary button" type="submit" value="Enviar" name="enviar">
-                    <input class="btn btn-primary button" type="reset" value="Restablecer">
-            </fieldset>
-        </form>`);
-        //var parametros = localStorage.getItem("carr");
-        $.ajax({            
-            data:  {"parametros": localStorage.getItem("carr")}, //datos que se envian a traves de ajax
-            url:   '../pages/correo.php', //archivo que recibe la peticion
-            type:  'post', //método de envio
-            beforeSend: function () {
-                    $("#resultado").html("Procesando, espere por favor...");
-            },
-            success:  function (response) { //una vez que el archivo recibe el request lo procesa y lo devuelve
-                    $("#resultado").html(response);
-            }
     });
-    });
+});
     
